b050d932cdb3b3828e0b7dffafe8b552c4f40212
shim <jhshim3478@gmail.com>
2018-12-29 23:37:55 +0900

    TIL algorithm 2 commit

--- algorithm/src/NumberTriangle.java
+++ algorithm/src/NumberTriangle.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class NumberTriangle {
+    public static void main(String args[]) {
+        int i, j, N = 1, L, NL = 1;
+
+        Scanner sc = new Scanner(System.in);
+
+        L = Integer.parseInt(sc.nextLine());
+
+        for (i = L; i >= 1; i--) {
+            for (j = 1; j <= i + 3; j++) {
+                System.out.print(" ");
+            }
+
+            for (j = 1; j <= NL; j++) {
+                System.out.print(N % 10 + " ");
+                N = N + 1;
+            }
+
+            System.out.println();
+
+            NL = NL + 1;
+        }
+    }
+
 }
---
+++ algorithm/src/PascalTriangle3.java
@@ -0,0 +1,28 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PascalTriangle3 {
+    static ArrayList binomial(final int N) {
+        BigInteger ret = BigInteger.ONE;
+        ArrayList<BigInteger> list = new ArrayList();
+        for (int k = 0; k <= N; k++) {
+            list.add(ret);
+            ret = ret.multiply(BigInteger.valueOf(N - k))
+                    .divide(BigInteger.valueOf(k + 1)); // ret *= (N - k) / (k + 1);
+        }
+        return list;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int p = Integer.parseInt(sc.nextLine());
+        for (int n = 0; n < p; n++) {
+            for (Object obj : binomial(n)) {
+                System.out.print(obj + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
