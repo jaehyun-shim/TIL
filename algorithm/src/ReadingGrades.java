970c195036ff2b81b53959751032060679ea2cf8
shim <jhshim3478@gmail.com>
2018-12-30 23:59:07 +0900

    TIL algorithm 2 commit

--- algorithm/src/ReadingGrades.java
+++ algorithm/src/ReadingGrades.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class ReadingGrades {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int[] NA, KO, EN, MA;
+        int N, B;
+
+
+    }
 }
