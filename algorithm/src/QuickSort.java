05093097ba749a3ca875c181d73cb08c104592b3
shim <jhshim3478@gmail.com>
2019-01-31 23:06:12 +0900

    algorithm commit 1

---
+++ algorithm/src/QuickSort.java
@@ -0,0 +1,49 @@
+public class QuickSort {
+    private int[] arrA;
+
+    public QuickSort(int[] arrA) {
+        this.arrA = arrA;
+    }
+
+    public void quickS(int low, int high) {
+        int mid = (low + high) / 2;
+        int left = low;
+        int right = high;
+        int pivot = arrA[mid];
+        while (left <= right) {
+            while (arrA[left] < pivot)
+                left++;
+            while (arrA[right] > pivot)
+                right--;
+            if (left <= right) {
+                int temp = arrA[left];
+                arrA[left] = arrA[right];
+                arrA[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        if (low < right)
+            quickS(low, right);
+        if (left < high)
+            quickS(left, high);
+    }
+
+    public void display() {
+        for (int i = 0; i < arrA.length; i++) {
+            System.out.print(" " + arrA[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = {6, 2, 4, 12, 10};
+        QuickSort i = new QuickSort(a);
+        System.out.print("UnSorted : ");
+        i.display();
+        i.quickS(0, a.length - 1);
+        System.out.println("");
+        System.out.print("Quick Sorted : ");
+        i.display();
+    }
+}
