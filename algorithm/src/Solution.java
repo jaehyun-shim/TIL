548effd53b36bdc11c609b90ca2b95f0cd723629
shim <jhshim3478@gmail.com>
2019-02-14 23:56:57 +0900

    algorithm commit

--- algorithm/src/Solution.java
+++ algorithm/src/Solution.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class Solution {
+class Solution {
+
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{29, 7, 34, 2};
+
+        ArrayList numbersList = new ArrayList();
+        StringBuilder numbersStr = new StringBuilder();
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbersStr.append(numbers[i]);
+
+
+        }
+
+
+        //numbersList.add(1);
+        System.out.println(numbersStr);
+
+    }
 }
--- algorithm/src/TheGreatestNumber.java
+++ algorithm/src/TheGreatestNumber.java
@@ -1,4 +1,67 @@
-package PACKAGE_NAME;
-
 public class TheGreatestNumber {
+    private static int[] arrA;
+
+    public TheGreatestNumber(int[] arrA) {
+        this.arrA = arrA;
+    }
+
+    public static void quickS(int low, int high) {
+        int mid = (low + high) / 2;
+        int left = low;
+        int right = high;
+        int pivot = arrA[mid];
+        while (left <= right) {
+            while (arrA[left] > pivot)
+                left++;
+            while (arrA[right] < pivot)
+                right--;
+            if (left <= right) {
+                int temp = arrA[left];
+                arrA[left] = arrA[right];
+                arrA[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        if (low < right)
+            quickS(low, right);
+        if (left < high)
+            quickS(left, high);
+    }
+
+    public String solution(int[] numbers) {
+        String answer = "";
+
+        answer = answerStringBuilder(numbers);
+
+        int[] answerChar = new int[answer.length()];
+
+        for (int i = 0; i < answerChar.length; i++) {
+            answerChar[i] = Integer.parseInt(answer.substring(i, i + 1));
+        }
+
+        answerConstructor(answerChar);
+
+        quickS(0, arrA.length - 1);
+
+        return answerStringBuilder(arrA);
+    }
+
+    static void answerConstructor(int[] numberc) {
+        new TheGreatestNumber(numberc);
+    }
+
+    static String answerStringBuilder(int[] numberArray) {
+        StringBuilder beforeValue = new StringBuilder();
+
+        for (int i = 0; i < numberArray.length; i++) {
+            String str = String.valueOf(numberArray[i]);
+            if (str != null) {
+                beforeValue.append(str);
+            }
+        }
+        return String.valueOf(beforeValue);
+    }
+
 }
