2624c5e7b981e72cbeac7979ece78f2402647257
shim <jhshim3478@gmail.com>
2019-03-23 23:22:05 +0900

    reactive update 1

--- java/reactiveStreams/src/PubSub.java
+++ java/reactiveStreams/src/PubSub.java
@@ -1,9 +1,10 @@
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by jhshim3478@gmail.com on 2019-03-19
@@ -12,19 +13,24 @@ import java.util.concurrent.TimeUnit;
  */
 public class PubSub {
     public static void main(String[] args) throws InterruptedException {
-        Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
+        // Iterable<Integer> itr = Arrays.asList(1, 2, 3, 4, 5);
         ExecutorService es = Executors.newSingleThreadExecutor();
 
         Flow.Publisher p = new Flow.Publisher() {
+            Iterable<Integer> iter = Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList());
+
             @Override
             public void subscribe(Flow.Subscriber subscriber) {
 
-                Iterator<Integer> it = itr.iterator();
+                Iterator<Integer> it = iter.iterator();
 
                 subscriber.onSubscribe(new Flow.Subscription() {
 
                     @Override
                     public void request(long n) {
+                        // iter.forEach(s -> subscriber.onNext(s));
+                        // subscriber.onComplete();
+
                         // Future<?> f = es.submit(() -> { // 중간에 작업을 취소할 수 있음.(비동기)
                         es.execute(() -> {
                             int i = 0;
